8a1d9b0d33e231079a04632c919a433509c3d31b
SchwiftyUI <schwiftyui@gmail.com>
2020-05-21 13:21:17 -0700

    Initial Commit

---
+++ ShapeSettings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShapeSettings
+//
+//  Created by SchwiftyUI on 5/21/20.
+//  Copyright © 2020 SchwiftyUI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
